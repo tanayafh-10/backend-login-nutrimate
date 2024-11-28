5f6aeebf0fa7536a744d79d5bfd060d44b8a9b95
tanayafh-10 <128033228+tanayafh-10@users.noreply.github.com>
2024-11-28 18:56:51 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    SECRET_KEY: 'c7a899adf87b667a51c75000a81ecc3b3d789fefc99be39ace09aef9219f8909',
+    PORT: 3000,
+    DB_HOST: '34.101.201.162',
+    DB_USER: 'root',
+    DB_PASSWORD: 'nutrimate',
+    DB_NAME: 'authLogin',
+
+};
+  
